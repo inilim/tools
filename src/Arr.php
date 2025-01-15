505ea05308b2d59d9a46e0488489d9963cb4512c
Dmitry <40324321+inilim@users.noreply.github.com>
2025-01-15 23:07:09 +0300

    ref

--- src/Arr.php
+++ src/Arr.php
@@ -211,6 +211,6 @@ namespace Inilim\Tool;
 final class Arr extends \Inilim\LazyMethod\LazyMethodAbstract
 {
     protected const NAMESPACE   = 'Inilim\Tool\Method\Arr',
-        PATH_TO_DIR             = \__DIR__ . '/Method/Arr',
+        PATH_TO_DIR             = __DIR__ . '/Method/Arr',
         ALIAS                   = [];
 }
