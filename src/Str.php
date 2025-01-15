b91551cb7c786f9fde4850cc358c17901cee2338
Dmitry <40324321+inilim@users.noreply.github.com>
2025-01-15 23:05:59 +0300

    ref

--- src/Str.php
+++ src/Str.php
@@ -265,6 +265,6 @@ namespace Inilim\Tool;
 final class Str extends \Inilim\LazyMethod\LazyMethodAbstract
 {
     protected const NAMESPACE   = 'Inilim\Tool\Method\String',
-        PATH_TO_DIR             = __DIR__ . '/Method/String',
+        PATH_TO_DIR             = \__DIR__ . '/Method/String',
         ALIAS                   = [];
 }
