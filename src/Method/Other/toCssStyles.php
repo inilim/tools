2663c5e9608999e05e224b128f2477ecf2714e26
Dmitry <40324321+inilim@users.noreply.github.com>
2025-03-30 23:57:07 +0300

    add Other::toCssStyles

---
+++ src/Method/Other/toCssStyles.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Inilim\Tool\Method\Other;
+
+/**
+ * @author Laravel
+ * Conditionally compile styles from an array into a style list.
+ *
+ * @param array $array
+ * @return string
+ */
+function toCssStyles(array $array)
+{
+    $styles = [];
+
+    foreach ($array as $class => &$constraint) {
+        if (\is_numeric($class)) {
+            $styles[] = \Inilim\Tool\Method\Str\finish($constraint, ';');
+        } elseif ($constraint) {
+            $styles[] = \Inilim\Tool\Method\Str\finish($class, ';');
+        }
+    }
+
+    return \implode(' ', $styles);
+}
