c96d4b1f2cb816762594ea2062f5c94d1cb37d3f
Dmitry <40324321+inilim@users.noreply.github.com>
2025-03-30 20:40:38 +0300

    add Arr::select

---
+++ src/Method/Arr/select.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Inilim\Tool\Method\Arr;
+
+/**
+ * @author Laravel
+ * Select an array of values from an array.
+ *
+ * @param  array  $array
+ * @param  array|string  $keys
+ * @return array
+ */
+function select(array $array, $keys)
+{
+    $keys = \Inilim\Tool\Method\Arr\wrap($keys);
+
+    return \Inilim\Tool\Method\Arr\map($array, static function ($item) use ($keys) {
+        $result = [];
+
+        foreach ($keys as $key) {
+            if (\Inilim\Tool\Method\Arr\accessible($item) && \Inilim\Tool\Method\Arr\exists($item, $key)) {
+                $result[$key] = $item[$key];
+            } elseif (\is_object($item) && isset($item->{$key})) {
+                $result[$key] = $item->{$key};
+            }
+        }
+
+        return $result;
+    });
+}
