b8e0df909640decdc77094ae848d429f640d852b
Dmitry <40324321+inilim@users.noreply.github.com>
2025-03-30 20:23:41 +0300

    add Arr::toObj

---
+++ src/Method/Arr/toObj.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Inilim\Tool\Method\Arr;
+
+/**
+ * @author nette/utils
+ * Copies the elements of the $array array to the $object object and then returns it.
+ * @template T of object
+ * @param  T  $object
+ * @return T
+ */
+function toObj(iterable $array, object $object)
+{
+    foreach ($array as $k => &$v) {
+        $object->$k = $v;
+    }
+
+    return $object;
+}
