6ecc1655493c60c3185cf1860094642983e585f0
Dmitry <40324321+inilim@users.noreply.github.com>
2025-04-13 19:50:27 +0300

    fix tests

--- tests/Method/Str/limitTest.php
+++ tests/Method/Str/limitTest.php
@@ -9,18 +9,18 @@ class limitTest extends TestCase
 {
     function test()
     {
-        $this->assertSame('Explosion is...', Str::limit('Explosion is a free, open source PHP web application tools.', 10));
+        $this->assertSame('Laravel is...', Str::limit('Laravel is a free, open source PHP web application framework.', 10));
         $this->assertSame('这是一...', Str::limit('这是一段中文', 6));
-        $this->assertSame('Explosion is a...', Str::limit('Explosion is a free, open source PHP web application tools.', 15, '...', true));
+        $this->assertSame('Laravel is a...', Str::limit('Laravel is a free, open source PHP web application framework.', 15, '...', true));
 
-        $string = 'The PHP tools for web artisans.';
+        $string = 'The PHP framework for web artisans.';
         $this->assertSame('The PHP...', Str::limit($string, 7));
         $this->assertSame('The PHP...', Str::limit($string, 10, '...', true));
         $this->assertSame('The PHP', Str::limit($string, 7, ''));
         $this->assertSame('The PHP', Str::limit($string, 10, '', true));
-        $this->assertSame('The PHP tools for web artisans.', Str::limit($string, 100));
-        $this->assertSame('The PHP tools for web artisans.', Str::limit($string, 100, '...', true));
-        $this->assertSame('The PHP tools...', Str::limit($string, 20, '...', true));
+        $this->assertSame('The PHP framework for web artisans.', Str::limit($string, 100));
+        $this->assertSame('The PHP framework for web artisans.', Str::limit($string, 100, '...', true));
+        $this->assertSame('The PHP framework...', Str::limit($string, 20, '...', true));
 
         $nonAsciiString = '这是一段中文';
         $this->assertSame('这是一...', Str::limit($nonAsciiString, 6));
