bf21c5956e6403e6e2246b8f19b1620714916fd1
inilim <40324321+inilim@users.noreply.github.com>
2026-05-04 13:47:54 +0300

    add comments

--- src/Method/Str/length_m2.php
+++ src/Method/Str/length_m2.php
@@ -13,3 +13,31 @@ function length_m2(string $value): int
 {
     return (int)\preg_match_all('/.{1}/us', $value);
 }
+
+
+
+// $res = Other::timedMsCall(static function () {
+//     for ($i = 0; $i < 1_000_000; $i++) {
+//         $str = 'foo bar baz';
+//         \mb_strlen($str, 'UTF-8');
+//         // \length_m2($str);
+//         // \length_m3($str);
+//         $str = 'foo bar' . "\n" . 'baz';
+//         \mb_strlen($str, 'UTF-8');
+//         // \length_m2($str);
+//         // \length_m3($str);
+//         $str = 'こんにちは';
+//         \mb_strlen($str, 'UTF-8');
+//         // \length_m2($str);
+//         // \length_m3($str);
+//         $str = 'こん' . "\n" . 'にちは';
+//         \mb_strlen($str, 'UTF-8');
+//         // \length_m2($str);
+//         // \length_m3($str);
+//     }
+// });
+
+// 1 млн итераций
+// mb_strlen 300ms
+// length_m2 > preg_match_all 1700ms
+// length_m3 > preg_replace 2500ms
